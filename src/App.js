204d17b0f2502ff0b7ad5520e819bb98b2738170
szergej911 <nemeth.gergely22@gmail.com>
2023-12-15 09:47:24 +0100

    SSetting up routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import "./App.css";
-import Landing from "./components/Landing";
 import Cart from "./components/Cart";
 import Profile from "./components/Profile";
 import Tickets from "./components/Tickets";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <div>
       <Header />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/tickets" element={<Tickets />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
-      <Navbar>
-        <Routes>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/tickets" element={<Tickets />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </Navbar>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
-  return <div></div>;
+  return (
+    <div className="navbar">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <ul>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/tickets">Tickets</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
