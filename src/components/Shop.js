85eff6f44af2ac84966e7511969112bf8760f93a
szergej911 <nemeth.gergely22@gmail.com>
2024-01-09 11:47:28 +0100

    Adding state to the Shop  Component

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import Ticket from "./Ticket";
 
 export default function Shop() {
+  const [ticket, setticket] = useState(0);
+
+  function handleClick() {
+    setticket(ticket + 1);
+  }
+
   return (
     <>
       <p>Ticket & passes</p>
@@ -8,7 +15,14 @@ export default function Shop() {
       <button type="button">Passes</button>
       <div className="tickets">
         <Ticket type="One day ticket" usability={24} price={900} />
+        <button type="button" onClick={handleClick}>
+          Add to cart
+        </button>
+
         <Ticket type="Two day ticket" usability={48} price={1800} />
+        <button type="button" onClick={handleClick}>
+          Add to cart
+        </button>
       </div>
     </>
   );
--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,13 +1,5 @@
-import { useState } from "react";
-
 // eslint-disable-next-line react/prop-types
 export default function Ticket({ type, usability, price }) {
-  const [ticket, setTicket] = useState(0);
-
-  function handleClick() {
-    setTicket(ticket + 1);
-  }
-
   return (
     <ul className="shop-list">
       <li>
@@ -15,9 +7,6 @@ export default function Ticket({ type, usability, price }) {
         <p>You can use this ticket for {usability} hour</p>
         <p>{price} Ft</p>
         <p>{usability} h</p>
-        <button type="button" onClick={handleClick}>
-          Add to cart
-        </button>
       </li>
     </ul>
   );
