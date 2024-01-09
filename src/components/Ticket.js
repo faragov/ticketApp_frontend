5e6348135116f443b7880b2a79a562f737ebf897
szergej911 <nemeth.gergely22@gmail.com>
2024-01-09 10:29:57 +0100

    Adding state to the Ticket Component

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
+
 // eslint-disable-next-line react/prop-types
 export default function Ticket({ type, usability, price }) {
+  const [ticket, setTicket] = useState(0);
+
+  function handleClick() {
+    setTicket(ticket + 1);
+  }
+
   return (
     <ul className="shop-list">
       <li>
         <p>{type}</p>
         <p>You can use this ticket for {usability} hour</p>
-        <p>{price}</p>
+        <p>{price} Ft</p>
         <p>{usability} h</p>
-        <button type="button">Add to cart</button>
+        <button type="button" onClick={handleClick}>
+          Add to cart
+        </button>
       </li>
     </ul>
   );
