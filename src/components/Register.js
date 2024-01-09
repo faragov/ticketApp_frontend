726c7a85943b43d05daa69a680923cfa114018a3
Farago Vivien <faragovivien@gmail.com>
2024-01-09 18:42:02 +0100

    style(): fix styling with prettier

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,70 +1,73 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Register() {
-    const [user, setUser] = useState({
-        name: 'Username',
-        email: 'Email',
-        password: 'Password'
+  const [user, setUser] = useState({
+    name: "Username",
+    email: "Email",
+    password: "Password",
+  });
+
+  function handleChange(e) {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
     });
+  }
 
-    function handleChange(e) {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value
-        });
-    }
+  // Email is already taken logic
 
-    // Email is already taken logic
-
-    return (
-        <div className='form'>
-            <form onSubmit={e => {
-                e.preventDefault();
-                if (!user.name || !user.email || !user.password) {
-                    alert('Name, email, and password are required.');
-                } else if (user.password.length < 8) {
-                    alert('Password must be at least 8 characters.');
-                } else {
-                    alert('Submitting!');
-                }
-            }}
-            >
-                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                <label>
-                    <FontAwesomeIcon icon={faUser} />
-                    <input
-                        id="name"
-                        name="name"
-                        value={user.name}
-                        onChange={handleChange}
-                    />
-                </label>
-                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                <label>
-                    <FontAwesomeIcon icon={faEnvelope} />
-                    <input
-                        id="email"
-                        name="email"
-                        value={user.email}
-                        onChange={handleChange}
-                    />
-                </label>
-                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                <label>
-                    <FontAwesomeIcon icon={faLock} />
-                    <input
-                        id="password"
-                        name="password"
-                        value={user.password}
-                        onChange={handleChange}
-                    />
-                </label>
-                <button type="submit">Sign Up</button>
-            </form>
-            {/* Need to change to Login */}
-            <p>Are you a member? <a href="./Landing">Log in</a></p>
-        </div>
-    );
+  return (
+    <div className="form">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!user.name || !user.email || !user.password) {
+            alert("Name, email, and password are required.");
+          } else if (user.password.length < 8) {
+            alert("Password must be at least 8 characters.");
+          } else {
+            alert("Submitting!");
+          }
+        }}
+      >
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label>
+          <FontAwesomeIcon icon={faUser} />
+          <input
+            id="name"
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+          />
+        </label>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label>
+          <FontAwesomeIcon icon={faEnvelope} />
+          <input
+            id="email"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+        </label>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label>
+          <FontAwesomeIcon icon={faLock} />
+          <input
+            id="password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign Up</button>
+      </form>
+      {/* Need to change to Login */}
+      <p>
+        Are you a member? <a href="./Landing">Log in</a>
+      </p>
+    </div>
+  );
 }
