8170de1aa730ff751f39975ca85489a23248263e
Károly András Kovács <karoly.kovacs@greenfoxacademy.com>
2024-01-11 11:21:08 +0100

    fix(components/Shop): moved the intial data and renamed some variables

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,48 +1,53 @@
 import { useState } from "react";
 import Ticket from "./Ticket";
 
-export default function Shop() {
-  const [ticket] = useState([
-    {
-      id: 1,
-      name: "One day ticket",
-      price: 900,
-      duration: "24 h",
-      description: "You can use this ticket for 24 hour",
-      type: "ticket",
-    },
-    {
-      id: 2,
-      name: "Two day ticket",
-      price: 1800,
-      duration: "48 h",
-      description: "You can use this ticket for 48 hour",
-      type: "ticket",
-    },
-    {
-      id: 3,
-      name: "15 day pass",
-      price: 6300,
-      duration: "Two-day pass",
-      description: "You can use this pass for 15 days",
-      type: "pass",
-    },
-    {
-      id: 4,
-      name: "30 day pass",
-      price: 9500,
-      duration: "24 h",
-      description: "You can use this pass for 15 days",
-      type: "pass",
-    },
-  ]);
+const initialTickets = [
+  {
+    id: 1,
+    name: "One day ticket",
+    price: 900,
+    duration: "24 h",
+    description: "You can use this ticket for 24 hour",
+    type: "ticket",
+  },
+  {
+    id: 2,
+    name: "Two day ticket",
+    price: 1800,
+    duration: "48 h",
+    description: "You can use this ticket for 48 hour",
+    type: "ticket",
+  },
+  {
+    id: 3,
+    name: "15 day pass",
+    price: 6300,
+    duration: "Two-day pass",
+    description: "You can use this pass for 15 days",
+    type: "pass",
+  },
+  {
+    id: 4,
+    name: "30 day pass",
+    price: 9500,
+    duration: "24 h",
+    description: "You can use this pass for 15 days",
+    type: "pass",
+  },
+];
 
-  const ticketType = ticket.map((tickets) => (
+export default function Shop() {
+  // !!!! delete the following comment if the setTicket is used !!!!
+  // eslint-disable-next-line no-unused-vars
+  const [tickets, setTickets] = useState(initialTickets);
+
+  const ticketMap = tickets.map((ticket) => (
     <Ticket
-      type={tickets.name}
-      description={tickets.description}
-      usability={tickets.duration}
-      price={tickets.price}
+      key={ticket.id}
+      type={ticket.name}
+      description={ticket.description}
+      usability={ticket.duration}
+      price={ticket.price}
     />
   ));
 
@@ -51,7 +56,7 @@ export default function Shop() {
       <header>Ticket & passes</header>
       <button type="button">Tickets</button>
       <button type="button">Passes</button>
-      <div>{ticketType}</div>
+      <div>{ticketMap}</div>
     </>
   );
 }
