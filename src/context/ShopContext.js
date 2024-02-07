bacf506080e97d7d98c8843642eb8c27a7bfe7eb
Károly András Kovács <karoly.kovacs@greenfoxacademy.com>
2024-02-07 15:06:46 +0100

    feat(): move ticket converter into separate file

--- src/context/ShopContext.js
+++ src/context/ShopContext.js
@@ -5,6 +5,7 @@ import {
   postPurchase,
   updatePurchase,
 } from "../services/CartService";
+import { getCartMap, getCartItems } from "../services/TicketConverter";
 import { useAuth } from "../hooks/AuthContext";
 import getAllTickets from "../services/ShopService";
 
@@ -22,42 +23,6 @@ function ShopProvider({ children }) {
 
   const getPurchaseIds = () =>
     pendingPurchases.flatMap((purchase) => purchase.purchases);
-
-  const getCartMap = (purchases) =>
-    purchases.reduce((acc, curr) => {
-      const { id: purchaseId, ticket } = curr;
-      const { id: ticketId, name, price, type, duration } = ticket;
-
-      if (!acc[ticketId]) {
-        acc[ticketId] = [];
-      }
-
-      acc[ticketId].push({ purchaseId, name, price, type, duration });
-
-      return acc;
-    }, {});
-
-  const getCartItems = (cartMap) =>
-    Object.entries(cartMap).map(([ticketId, purchases]) => {
-      const totalPrice = purchases.reduce(
-        (total, { price }) => total + price,
-        0,
-      );
-      const amount = purchases.length;
-
-      const { name, type, duration } = purchases[0];
-
-      return {
-        ticketId,
-        name,
-        price: totalPrice / amount,
-        totalPrice,
-        amount,
-        type,
-        duration,
-        purchases: purchases.map(({ purchaseId }) => purchaseId),
-      };
-    });
 
   const fetchPurchases = () =>
     getPendingPurchases(token).json((json) =>
